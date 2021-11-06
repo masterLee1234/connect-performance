43481ee7a388aad4373b51ad52a4765d6127ddab
Jin <lhjin@naver.com>
2021-11-06 23:59:53 +0900

    recieved

--- src/main/java/com/performance/connect/controller/MainController.java
+++ src/main/java/com/performance/connect/controller/MainController.java
@@ -1,13 +1,37 @@
 package com.performance.connect.controller;
 
+import com.performance.connect.domain.Performance;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/")
+@Controller
 public class MainController {
-    @GetMapping
-    public String index(Model model){
+    @GetMapping(value = "/")
+    public String index(Model model) {
         return "index";
+    }
+
+    @GetMapping(value = "/post_performance")
+    public String getPerformance() {
+        System.out.println("asdf");
+        return "post_performance";
+    }
+
+    @PostMapping(value = "/post_performance")
+    public String postPerformance(PerformanceForm form) {
+        Performance performance = new Performance();
+        performance.setSchool(form.getSchool());
+        performance.setGrade(form.getGrade());
+        performance.setCls(form.getCls());
+        performance.setSubject(form.getSubject());
+        performance.setDue(form.getDue());
+        performance.setTitle(form.getTitle());
+        performance.setDesc(form.getDesc());
+
+        performance.to_string();
+        return "redirect:/";
     }
 }
