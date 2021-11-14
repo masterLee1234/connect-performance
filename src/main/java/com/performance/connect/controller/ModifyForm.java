89d4f889110668f610eb3f706c10925ad945317f
sangjin <qotkdwls88@naver.com>
2021-11-14 15:49:51 +0900

    updated by sangjin

---
+++ src/main/java/com/performance/connect/controller/ModifyForm.java
@@ -0,0 +1,34 @@
+package com.performance.connect.controller;
+
+public class ModifyForm {
+  private String updated
+  private String due;
+  private String title;
+  private String desc;
+
+  public String getUpdated(){
+    return updated;
+  }
+  public void setUpdated(String updated){
+    this.updated = updated;
+  }
+  public String getDue(){
+    return due;
+  }
+  public void setDue(String due){
+    this.due = due;
+  }
+  public String getTitle(){
+    return title;
+  }
+  public void setTitle(String title){
+    this.title = title;
+  }
+  public String getDesc(){
+    return desc;
+  }
+  public void setDesc(String desc){
+    this.desc = desc;
+  }
+
+}
---
+++ src/main/java/com/performance/connect/controller/UserDataForm.java
@@ -0,0 +1,25 @@
+package com.performance.connect.controller;
+
+public class UserDataForm {
+  private String school;
+  private int grade;
+  private int cls;
+  public String getSchool(){
+    return school
+  }
+  public void setSchool(String school){
+    this.school = school;
+  }
+  public String getGrade(){
+    return grade
+  }
+  public void setGrade(String grade){
+    this. grade= grade;
+  }
+  public String getCls(){
+    return cls
+  }
+  public void setCls(String cls){
+    this.cls = cls;
+  }
+}
