db92ff945a27f3bfcd25f138bde36a5200387880
jin <lhjin0j@naver.com>
2021-11-22 10:18:45 +0900

    updated

---
+++ src/main/resources/ddl.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS performance;
+
+CREATE TABLE performance(
+    `id` varchar(200) NOT NULL,
+    `school` varchar(100) NOT NULL,
+    `grade` int NOT NULL,
+    `cls` int NOT NULL,
+    `subject` varchar(100) NOT NULL,
+    `created` DATETIME NOT NULL,
+    `updated` DATETIME NOT NULL,
+    `due` DATETIME NOT NULL,
+    `title` varchar(200) NOT NULL,
+    `description` TEXT  NOT NULL,
+    PRIMARY KEY(`id`)
+);
